393cd4ec789f07b0156e851bc8dd6d5cb9f2ffe7
Corey Cartmill <ct.cartmill@gmail.com>
2022-04-27 22:34:53 +1000

    Window Rename

--- src/maze/UserGUI.java
+++ src/maze/UserGUI.java
@@ -140,6 +140,6 @@ public class UserGUI extends JFrame implements ActionListener, Runnable{
     }
 
     public static void main (String[] args){
-        new UserGUI("UserGUIS");
+        new UserGUI("MazeCo Computer Assisted Maze Design Tool");
     }
 }
